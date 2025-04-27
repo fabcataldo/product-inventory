612c07acdd1dde432a3f60e16f36f4aec6cb3a02
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2025-04-27 18:03:42 -0300

    Cleaning product form logic fixed

--- src/app/pages/product/product-page.component.ts
+++ src/app/pages/product/product-page.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Product } from 'src/app/products/interfaces/product.interface';
 import { AppState } from 'src/app/shared/interfaces/app-state.interface';
+import { resetProduct } from 'src/store/products/products.actions';
 
 @Component({
   selector: 'app-product-page',
@@ -14,12 +15,14 @@ export class ProductPageComponent implements OnInit {
   isAddingProduct = false;
   product: Product | null = null;
   loading = false;
+  isGoingLastPage = false;
+
   constructor(private store: Store<AppState>, private location: Location) {}
 
   ngOnInit(): void {
     this.loading = true;
     this.store.subscribe((store) => {
-      if (store) {
+      if (store && !this.isGoingLastPage) {
         this.loading = false;
         this.product = this.isAddingProduct ? null : store.products!.product!;
         this.isEditingProduct = store.products.isEditingProduct;
@@ -29,6 +32,8 @@ export class ProductPageComponent implements OnInit {
   }
 
   goBack(): void {
+    this.isGoingLastPage = true;
+    this.store.dispatch(resetProduct());
     this.location.back();
   }
 }
