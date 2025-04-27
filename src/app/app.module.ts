4381e0212bc7ad3c0b958e112bf398b8bbc4c782
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2025-04-27 18:02:16 -0300

    ProductsModule, OperationsModule, CategoriesModule, modules added in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,26 @@ import { productsReducer } from 'src/store/products/products.reducer';
 import { dashboardReducer } from 'src/store/dashboard/dashboard.reducer';
 import { DashboardEffects } from 'src/store/dashboard/dashboard.effects';
 import { SharedComponentsModule } from './shared/shared-components.module';
+import { ProductsModule } from './products/products.module';
+import { OperationsModule } from './operations/operations.module';
+import { CategoriesModule } from './categories/categories.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     StoreModule.forRoot({
       products: productsReducer,
       dashboard: dashboardReducer,
     }),
     EffectsModule.forRoot([ProductsEffects, DashboardEffects]),
+    ProductsModule,
+    OperationsModule,
     SharedComponentsModule,
+    CategoriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
